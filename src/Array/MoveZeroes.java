5de9b6db2c93376c064674e07ab95f7aedf3482a
18645956947 <18645956947@163.com>
2019-03-15 15:58:45 +0800

    283. 移动零

--- src/Array/MoveZeroes.java
+++ src/Array/MoveZeroes.java
@@ -1,11 +1,11 @@
 package Array;
 
-import zhx.day1213.Solution_01;
 
 /** 
 * @author ZHX
 * @date 2019年3月15日下午1:32:14 
 * @Description: 283. 移动零
+ *
  *  给定一个数组 nums，编写一个函数将所有 0 移动到数组的末尾
  *  同时保持非零元素的相对顺序。
  * 维护一个INDEX 用来记录非零元素
@@ -26,7 +26,7 @@ public class MoveZeroes {
 	}
 	public static void main(String[] args) {
 		int[] num = new int[] {0,1,0,3,12};
-		Solution_01 d2 = new Solution_01();
+		MoveZeroes d2 = new MoveZeroes();
 		d2.moveZeroes(num);
 	}
 }
