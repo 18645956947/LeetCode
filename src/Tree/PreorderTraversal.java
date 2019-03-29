92925b8b291c4ae30b536088ab49d58bdead9c94
18645956947 <18645956947@163.com>
2019-03-29 13:33:14 +0800

    144. 二叉树的前序遍历

--- src/Tree/PreorderTraversal.java
+++ src/Tree/PreorderTraversal.java
@@ -1,11 +1,15 @@
 package Tree;
 
+import sun.reflect.generics.tree.Tree;
+
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Stack;
 
 /**
  * 144. 二叉树的前序遍历
  * @author zhx
+ * 递归实现树的先序遍历
  */
 public class PreorderTraversal {
     public List<Integer> preorderTraversal(TreeNode root) {
@@ -25,4 +29,31 @@ public class PreorderTraversal {
             printVal(node.right, list);
         }
     }
+
+
+    /**
+     * 迭代实现树的先序遍历
+     * @param root
+     * @return
+     */
+    public List<Integer> preorderTraversa2(TreeNode root) {
+        List<Integer> list = new ArrayList<>();
+        if(root == null) return list;
+        Stack<TreeNode> stack = new Stack<TreeNode>();
+        stack.push(root);
+        while (!stack.isEmpty()){
+            TreeNode top = stack.pop();
+            list.add(top.val);
+            if(top.right!=null){
+                stack.push(top.right);
+            }
+            if(top.left!=null){
+                stack.push(top.left);
+            }
+        }
+        return list;
+    }
+
+
+
 }
