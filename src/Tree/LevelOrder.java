499d8be49cdf9712716346b98f77ffe859f01d74
18645956947 <18645956947@163.com>
2019-04-04 11:42:04 +0800

    102. 二叉树的层次遍历

--- src/Tree/LevelOrder.java
+++ src/Tree/LevelOrder.java
@@ -1,5 +1,8 @@
 package Tree;
 
+import sun.reflect.generics.tree.Tree;
+
+import java.beans.IntrospectionException;
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
@@ -10,6 +13,11 @@ import java.util.List;
  *
  */
 public class LevelOrder {
+    /**
+     * 递归操作
+     * @param root
+     * @return
+     */
     public List<List<Integer>> levelOrder(TreeNode root) {
         List<List<Integer>> result = new ArrayList<List<Integer>>();
         if(root==null){
@@ -31,6 +39,36 @@ public class LevelOrder {
                 }
             }
             result.add(tem);
+        }
+        return result;
+    }
+
+    /**
+     * 非递归实现二叉树的层序遍历
+     * @param root 树的根节点
+     * @return 一个[]
+     */
+    public List<List<Integer>> levelOrder_1(TreeNode root){
+        List<List<Integer>> result = new ArrayList<>();
+        List<TreeNode> list = new ArrayList<>();
+        if (root == null) {
+            return result;
+        }
+        list.add(root);
+        while (!list.isEmpty()){
+            List<TreeNode> nextTreeNode = new ArrayList<TreeNode>();
+            List<Integer> curList = new ArrayList<>();
+            for(TreeNode  node : list){
+                curList.add(node.val);
+                if(node.left != null){
+                    nextTreeNode.add(node.left);
+                }
+                if(node.right != null){
+                    nextTreeNode.add(node.right);
+                }
+            }
+            list = nextTreeNode;
+            result.add(curList);
         }
         return result;
     }
