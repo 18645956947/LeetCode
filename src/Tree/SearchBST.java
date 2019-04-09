c7f0a4252a5a51009d9d7baf99f38889cb02b0d3
18645956947 <18645956947@163.com>
2019-04-09 19:57:45 +0800

    700. 二叉搜索树中的搜索

---
+++ src/Tree/SearchBST.java
@@ -0,0 +1,15 @@
+package Tree;
+
+/**
+ * 700. ¶þ²æËÑË÷Ê÷ÖÐµÄËÑË÷
+ * @author zhx
+ *
+ */
+public class SearchBST {
+    public TreeNode searchBST(TreeNode root, int val) {
+        if(root == null){
+            return null;
+        }
+        return root;
+    }
+}
