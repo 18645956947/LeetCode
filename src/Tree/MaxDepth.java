964616ca043330312bbc05a508263905dde25f77
18645956947 <18645956947@163.com>
2019-04-12 11:41:08 +0800

    559. N叉树的最大深度

--- src/Tree/MaxDepth.java
+++ src/Tree/MaxDepth.java
@@ -23,11 +23,25 @@ public class MaxDepth {
         }
     }
 
-    public static void main(String[] args){
-        List<Node> list = new ArrayList<>();
 
-        Node node = new Node();
-        list.add(node);
-        Node node1 = new Node(1, list);
+    public static void main(String[] args){
+        Node child5 = new Node(11,null);
+        Node child6 = new Node(12,null);
+        Node child7 = new Node(21,null);
+        List<Node> list2 = new ArrayList<>();
+        list2.add(child5);
+        list2.add(child6);
+        list2.add(child7);
+        Node child2 = new Node(2, list2);
+        Node child3 = new Node(3, null);
+        Node child4 = new Node(4, null);
+        List<Node> list1 = new ArrayList<>();
+        list1.add(child2);
+        list1.add(child3);
+        list1.add(child4);
+        Node root = new Node(1, list1);
+
     }
+
 }
+
