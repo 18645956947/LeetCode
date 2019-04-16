d6e1127d427ec948ca133e0b013710db3477a6f1
18645956947 <18645956947@163.com>
2019-04-16 15:30:00 +0800

    437. 路径总和 III

---
+++ src/Tree/PathSum_01.java
@@ -0,0 +1,29 @@
+package Tree;
+
+import sun.reflect.generics.tree.Tree;
+
+/**
+ * 437. Â·¾¶×ÜºÍ III
+ * @author zhx
+ */
+public class PathSum_01 {
+    public int pathSum(TreeNode root, int sum) {
+        if(root == null){
+            return 0;
+        }
+        int res = findPath(root, sum);
+        return res;
+    }
+    private int findPath(TreeNode node, int sum){
+        if(node == null){
+            return 0;
+        }
+        int res = 0;
+        if(node.val == sum){
+            res += 1;
+        }
+        res += findPath(node.left, sum - node.val);
+        res += findPath(node.right, sum - node.val);
+        return res;
+    }
+}
