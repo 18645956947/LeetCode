242372df940a1a68514ba30704f1599b82a7b5ff
18645956947 <18645956947@163.com>
2019-05-07 10:33:50 +0800

    343. 整数拆分

--- src/Dynamic_Programming/IntegerBreak.java
+++ src/Dynamic_Programming/IntegerBreak.java
@@ -38,4 +38,26 @@ public class IntegerBreak {
     private int max3(int a, int b, int c){
         return Math.max(a, Math.max(b, c));
     }
+
+    /**
+     * 动态规划 自底向上
+     * @param n
+     * @return
+     */
+    public int integerBreak_02(int n) {
+        if(n < 1){
+            return -1;
+        }
+        memo = new int[n+1];
+        memo[1] = 1;
+
+        for(int i = 2;i<=n;i++){
+            for(int j = 1;j<=i-1;j++){
+                //j + i-j
+                memo[i] = max3(memo[i], j * (i-j), j * memo[i-j]);
+
+            }
+        }
+        return memo[n];
+    }
 }
