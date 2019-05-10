38f225748e2530f04054650a6249330b7c69934b
18645956947 <18645956947@163.com>
2019-05-10 19:15:27 +0800

    342. 4的幂

--- src/Array/IsPowerOfFour.java
+++ src/Array/IsPowerOfFour.java
@@ -6,21 +6,9 @@ package Array;
  */
 public class IsPowerOfFour {
     public boolean isPowerOfFour(int num) {
-        if(num == 0){
-            return false;
-        }
-        else if(num == 1){
-            return true;
-        }
-        else {
-            while(true){
-                if(num % 4 == 0){
-                    num /= 4;
-                    return isPowerOfFour(num);
-                }
-                else return false;
-            }
-
-        }
+        if(num == 1) return true;
+        if(num == 0 || num==2 || num==3 || num % 4 != 0) return false;
+        num /= 4;
+        return isPowerOfFour(num);
     }
 }
