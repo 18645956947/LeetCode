3b968b364074dc6f809ad375cebefc8b577266a7
18645956947 <18645956947@163.com>
2019-05-30 19:29:50 +0800

    704. 二分查找

---
+++ src/Binary_Search/Search.java
@@ -0,0 +1,24 @@
+package Binary_Search;
+
+/**
+ * 704. 二分查找
+ * @author zhx
+ */
+public class Search {
+    public int search(int[] nums, int target) {
+        int l = 0, r = nums.length-1;//维护一个[l...r] 范围里寻找target
+        while(l <= r){  //当 l == r 时仍然有效
+            int mid = (l + r)/2;
+            if(nums[mid] == target){
+                return mid;
+            }
+            if(nums[mid] > target){
+                r = mid - 1;
+            }
+            else{
+                l = mid + 1;
+            }
+        }
+        return -1;
+    }
+}
