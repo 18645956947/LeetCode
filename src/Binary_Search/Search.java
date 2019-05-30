0acf8234033367cdc51b17ebe31be5b786a74288
18645956947 <18645956947@163.com>
2019-05-30 19:42:06 +0800

    704. 二分查找

--- src/Binary_Search/Search.java
+++ src/Binary_Search/Search.java
@@ -8,7 +8,7 @@ public class Search {
     public int search(int[] nums, int target) {
         int l = 0, r = nums.length-1;//维护一个[l...r] 范围里寻找target
         while(l <= r){  //当 l == r 时仍然有效
-            int mid = (l + r)/2;
+            int mid = (l + r)/2; //这里容易出现整型溢出问题可以写成 int mid = l + (r - l)/2; 变成减法
             if(nums[mid] == target){
                 return mid;
             }
