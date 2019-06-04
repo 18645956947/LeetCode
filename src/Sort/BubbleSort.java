31c7323707d626b6e06c13ff0ed0a172a4edf5b8
18645956947 <18645956947@163.com>
2019-06-04 10:57:12 +0800

    冒泡排序

--- src/Sort/BubbleSort.java
+++ src/Sort/BubbleSort.java
@@ -1,36 +1,39 @@
 package Sort;
 
+import java.util.Arrays;
+
 /**
  * @author lenovo
  * @date 2019年3月14日下午10:32:58
  * @Description:
  */
 public class BubbleSort {
-	// 冒泡排序
-	public static int[] bubbleSort(int[] sum) {
-		int tem = 0;
-		for (int i = 0; i < sum.length - 1; i++) {
-			for (int j = 0; j < sum.length - 1 - i; j++) {
-				if (sum[j] > sum[j + 1]) {
-					tem = sum[j + 1];
-					sum[j + 1] = sum[j];
-					sum[j] = tem;
+	
+	//冒泡排序
+	public static int[] bubble(int[] nums){
+		int temp = 0;
+		for (int i = 0; i < nums.length-1; i++) {
+			for (int j = 0; j < nums.length-i; j++) {
+				if(nums[i] < nums[j]){
+					swap(nums[i], nums[j]);
 				}
 			}
 		}
-		return sum;
+		return nums;
+	}
+	//交换两个数
+	public static void swap(int a, int b){
+		int temp = 0;
+		temp = a;
+		a = b;
+		b = temp;
 	}
 	
 	  //冒泡排序
 	 public static void main(String[] args) {
-		 int[] sum = new int[] {3,2,1,4};
-		 for(int i = 0;i<sum.length;i++) {
-			  System.out.print(sum[i]); 
-		 }
-		 System.out.println("----");
+		 int[] sum = new int[] {4,3,3,2,1};
 		 bubbleSort(sum);
-		 for(int i = 0;i<sum.length;i++) {
-			 System.out.print(sum[i]);
-		 }
+		 System.out.println(Arrays.toString(bubble(sum)));
 	 }
+	 
 }
