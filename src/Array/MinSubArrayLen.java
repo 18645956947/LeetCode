089d6ca3f174cc2a4b4f71ce44f72079a4c46dd2
18645956947 <18645956947@163.com>
2019-06-11 09:31:30 +0800

    209. 长度最小的子数组

--- src/Array/MinSubArrayLen.java
+++ src/Array/MinSubArrayLen.java
@@ -1,5 +1,7 @@
 package Array;
 
+import java.util.Arrays;
+
 /**
  * 209. 长度最小的子数组
  * @author zhx
@@ -10,10 +12,10 @@ public class MinSubArrayLen {
         int l = 0;
         int r = -1;//nums[l...r] 维持的滑动窗口
         int res = nums.length+1;//结果存储
+        int sum = 0;//连续数组的和
 
-        int sum = 0;
         while( l < nums.length){
-            if( r+1 < nums.length && sum < s){
+            if( r+1 < nums.length && sum < s){ 
                 r++;
                 sum += nums[r];
             }
@@ -29,6 +31,10 @@ public class MinSubArrayLen {
             return 0;
         }
         return res;
+    }
 
+    public static void main(String[] args) {
+        int[] a = new int[10];
+        System.out.println("Arrays.toString(a) = " + Arrays.toString(a));
     }
 }
