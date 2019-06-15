0add16d76831bc13c76bdfc0fca80fa2028c26ca
18645956947 <18645956947@163.com>
2019-06-15 20:43:41 +0800

    剑指offer--合并两个排序的链表

--- src/Coding_Interviews/合并两个排序的链表.java
+++ src/Coding_Interviews/合并两个排序的链表.java
@@ -48,7 +48,25 @@ public class 合并两个排序的链表 {
         return head;
     }
 
-
+    //2.递归版本
+    public ListNode Merge_02(ListNode list1,ListNode list2) {
+        if(list1 == null){
+            return list2;
+        }
+        else if(list2 == null){
+            return list1;
+        }
+        ListNode res = null;
+        if(list1.val < list2.val){
+            res = list1;
+            res.next = Merge_02(list1.next, list2);
+        }
+        else {
+            res = list2;
+            res.next = Merge_02(list1, list2.next);
+        }
+        return res;
+    }
 
 
 }
