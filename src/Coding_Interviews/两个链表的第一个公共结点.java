994e867415afc3a6c960d4e23b3037498182e71b
18645956947 <18645956947@163.com>
2019-06-17 13:48:52 +0800

    剑指offer --两个链表的第一个公共结点

---
+++ src/Coding_Interviews/两个链表的第一个公共结点.java
@@ -0,0 +1,17 @@
+package Coding_Interviews;
+
+/**
+ * 剑指offer --两个链表的第一个公共结点
+ * @author zhx
+ */
+public class 两个链表的第一个公共结点 {
+    public ListNode FindFirstCommonNode(ListNode pHead1, ListNode pHead2) {
+        ListNode p = pHead1;
+        ListNode q = pHead2;
+        while(p != q){
+            p = (p == null ? pHead1 : p.next);
+            q = (q == null ? pHead2 : q.next);
+        }
+        return p;
+    }
+}
