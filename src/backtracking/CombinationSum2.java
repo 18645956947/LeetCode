802bfd305ce3fc40c90b3b57cda23a25c0c4353c
18645956947 <18645956947@163.com>
2019-06-19 11:27:36 +0800

    40. 组合总和 II

---
+++ src/backtracking/CombinationSum2.java
@@ -0,0 +1,37 @@
+package backtracking;
+
+import java.util.*;
+
+/**
+ * 40. 组合总和 II
+ * @author zhx
+ */
+public class CombinationSum2 {
+
+
+    List<List<Integer>> res = new ArrayList();
+    public List<List<Integer>> combinationSum2(int[] candidates, int target) {
+        Arrays.sort(candidates);
+        List<Integer> list = new ArrayList();
+        backtracking(list, candidates, target, 0);
+        //去重
+        Set<List<Integer>> set = new HashSet<>(res);
+        res = new ArrayList<>(set);
+        return res;
+    }
+    private void backtracking(List<Integer> list, int[] candidates, int target, int start){
+        if(target < 0){
+            return;
+        }
+        else if(target == 0){
+            res.add(new ArrayList(list));
+        }
+        else{
+            for(int i = start;i < candidates.length;i++){
+                list.add(candidates[i]);
+                backtracking(list, candidates, target - candidates[i], i+1);
+                list.remove(list.size() -1);
+            }
+        }
+    }
+}
