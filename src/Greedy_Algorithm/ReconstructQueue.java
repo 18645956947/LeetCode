d1f73a66c355e6e60d2728a2034323cb0a593558
18645956947 <18645956947@163.com>
2019-06-20 13:24:47 +0800

    406. 根据身高重建队列

---
+++ src/Greedy_Algorithm/ReconstructQueue.java
@@ -0,0 +1,34 @@
+package Greedy_Algorithm;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+
+/**
+ * 406. 根据身高重建队列
+ * @author zhx
+ */
+public class ReconstructQueue {
+
+    public int[][] reconstructQueue(int[][] people) {
+        if (0 == people.length || 0 == people[0].length)
+            return new int[0][0];
+        //按照身高降序 K升序排序
+        Arrays.sort(people, new Comparator<int[]>() {
+            @Override
+            public int compare(int[] o1, int[] o2) {
+                return o1[0] == o2[0] ? o1[1] - o2[1] : o2[0] - o1[0];
+            }
+        });
+        List<int[]> list = new ArrayList<>();
+        //K值定义为 排在h前面且身高大于或等于h的人数
+        //因为从身高降序开始插入，此时所有人身高都大于等于h
+        //因此K值即为需要插入的位置
+        for (int[] i : people) {
+            list.add(i[1], i);
+        }
+        return list.toArray(new int[list.size()][]);
+
+    }
+}
