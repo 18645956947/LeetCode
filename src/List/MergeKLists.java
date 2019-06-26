ca9b0a5c804611135ffa54f913d35bd30a7d4756
18645956947 <18645956947@163.com>
2019-06-26 10:01:20 +0800

    23. 合并K个排序链表

--- src/List/MergeKLists.java
+++ src/List/MergeKLists.java
@@ -34,4 +34,54 @@ public class MergeKLists {
         return head.next;
 
     }
+
+    //·ÖÖÎ·¨
+    public ListNode mergeKLists_02(ListNode[] lists) {
+        if(lists.length == 0){
+            return null;
+        }
+        int interval = 1;
+        while(interval < lists.length){
+            for(int i = 0;i + interval< lists.length;i=i+interval*2){
+                lists[i] = mergeTwoLists(lists[i], lists[i+interval]);
+            }
+            interval *= 2;
+        }
+        return lists[0];
+
+
+
+
+    }
+
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2){
+        ListNode h = new ListNode(0);
+        ListNode ans = h;
+
+        while(l1 != null && l2 != null){
+            if(l1.val < l2.val){
+                h.next = l1;
+                h = h.next;
+                l1 = l1.next;
+            }
+            else{
+                h.next = l2;
+                h = h.next;
+                l2 = l2.next;
+            }
+        }
+
+        if(l1 == null){
+            h.next = l2;
+        }
+
+        if(l2 == null){
+            h.next = l1;
+        }
+        return ans.next;
+
+    }
+
+
+
 }
