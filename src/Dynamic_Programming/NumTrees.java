11e70c1bdb31cb267fa65e65bbf3b21dcb95af39
18645956947 <18645956947@163.com>
2019-06-27 10:32:10 +0800

    96. 不同的二叉搜索树

---
+++ src/Dynamic_Programming/NumTrees.java
@@ -0,0 +1,21 @@
+package Dynamic_Programming;
+
+/**
+ * 96. 不同的二叉搜索树
+ * @author zhx
+ */
+public class NumTrees {
+
+    public int numTrees(int n) {
+        int[] dp = new int[n+1];
+        dp[0] = 1;
+        dp[1] = 1;
+        for(int i = 2;i<=n;i++){
+            for(int j = 1;j<i+1;j++){
+                dp[i] += dp[j-1]*dp[i-j];
+            }
+        }
+        return dp[n];
+
+    }
+}
