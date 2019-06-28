a9b7860040951d906917bd435f7d9d3aaf515418
18645956947 <18645956947@163.com>
2019-06-28 18:28:59 +0800

    剑指offer---链表中环的入口结点

---
+++ src/Coding_Interviews/链表中环的入口结点.java
@@ -0,0 +1,31 @@
+package Coding_Interviews;
+
+/**
+ * 剑指offer---链表中环的入口结点
+ * @author zhx
+ */
+public class 链表中环的入口结点 {
+
+    public ListNode EntryNodeOfLoop(ListNode pHead){
+        if(pHead == null || pHead.next == null || pHead.next.next == null){
+            return null;
+        }
+        ListNode first = pHead.next.next;
+        ListNode slow = pHead.next;
+        while(first != slow){
+            if(first.next.next != null && first.next != null){
+                first = first.next.next;
+                slow = slow.next;
+            }
+            else{
+                return null;
+            }
+        }
+        ListNode cur = pHead;
+        while(cur != slow){
+            slow = slow.next;
+            cur = cur.next;
+        }
+        return cur;
+    }
+}
