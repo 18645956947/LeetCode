e2067262edd8281d21ec1c781cfe53c05910ced3
18645956947 <18645956947@163.com>
2019-07-04 20:08:39 +0800

    剑指offer--数字在排序数组中出现的次数

---
+++ src/Coding_Interviews/第一个只出现一次的字符.java
@@ -0,0 +1,31 @@
+package Coding_Interviews;
+
+import java.util.LinkedHashMap;
+
+/**
+ * 剑指offer--第一个只出现一次的字符
+ * @au
+ */
+public class 第一个只出现一次的字符 {
+    public int FirstNotRepeatingChar(String str) {
+        LinkedHashMap<Character, Integer> map = new LinkedHashMap<Character, Integer>();
+        for(int i=0;i<str.length();i++){
+            if(map.containsKey(str.charAt(i))){
+                int time = map.get(str.charAt(i));
+                map.put(str.charAt(i), ++time);
+            }
+            else {
+                map.put(str.charAt(i), 1);
+            }
+        }
+        int pos = -1;
+        int i=0;
+        for(;i<str.length();i++){
+            char c = str.charAt(i);
+            if (map.get(c) == 1) {
+                return i;
+            }
+        }
+        return pos;
+    }
+}
