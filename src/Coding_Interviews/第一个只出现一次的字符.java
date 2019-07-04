06e885c8fd89b0b7a3cd2ea160392ffc55619894
18645956947 <18645956947@163.com>
2019-07-04 20:22:17 +0800

    剑指offer--第一个只出现一次的字符

--- src/Coding_Interviews/第一个只出现一次的字符.java
+++ src/Coding_Interviews/第一个只出现一次的字符.java
@@ -9,6 +9,7 @@ import java.util.LinkedHashMap;
 public class 第一个只出现一次的字符 {
 
     public int FirstNotRepeatingChar(String str) {
+
         LinkedHashMap<Character, Integer> map = new LinkedHashMap<Character, Integer>();
         for(int i=0;i<str.length();i++){
             if(map.containsKey(str.charAt(i))){
