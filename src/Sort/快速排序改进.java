9170bc6d6a043e7cfd23d47481ca89448fb372c7
18645956947 <18645956947@163.com>
2019-07-08 11:12:47 +0800

    快速排序改进-02随机快速排序

--- src/Sort/快速排序改进.java
+++ src/Sort/快速排序改进.java
@@ -8,6 +8,7 @@ public class 快速排序改进 {
 
     public static void quickSort(int[] arr, int L, int R){
         if(L < R){
+            swap(arr, L + (int)(Math.random() * (R - L + 1)), R);//加上这行就是随机快排时间复杂度为O（n*logn）
             int[] p = partition(arr, L, R);
             quickSort(arr, L, p[0]-1);
             quickSort(arr, p[1]+1, R);
@@ -15,19 +16,20 @@ public class 快速排序改进 {
     }
 
     private static int[] partition(int[] arr, int L, int R){
+        //其实就是[ <x | =x | >x ]   [less | =x... | more]
         int less = L-1;
-        int more = R;
+        int more = R+1;
+        int target = arr[R];
         while (L < more){
-            if(arr[L] < arr[R]){
+            if(arr[L] < target){
                 swap(arr, L++, ++less);
-            }else if(arr[L] > arr[R]){
+            }else if(arr[L] > target){
                 swap(arr, L, --more);
             }else{
                 L++;
             }
         }
-        swap(arr, R, more);
-        return new int[]{less + 1, more};
+        return new int[]{less + 1, more-1};
     }
 
     public static void swap(int[] arr, int i, int j){
