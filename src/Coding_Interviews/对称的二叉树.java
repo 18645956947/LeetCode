edabb3ba81fc6cc9aed02b632fb063068e0c56b2
18645956947 <18645956947@163.com>
2019-07-10 09:17:16 +0800

    剑指offer --- 对称的二叉树

---
+++ src/Coding_Interviews/对称的二叉树.java
@@ -0,0 +1,26 @@
+package Coding_Interviews;
+
+/**
+ * 剑指offer --- 对称的二叉树
+ * @author zhx
+ */
+public class 对称的二叉树 {
+
+    //递归版本
+    boolean isSymmetrical(TreeNode pRoot)
+    {
+        if(pRoot == null){
+            return true;
+        }
+        return isSymmetrical(pRoot.left, pRoot.right);
+    }
+    private boolean isSymmetrical(TreeNode left, TreeNode right){
+        if(left == null && right == null){
+            return true;
+        }
+        if(left == null || right == null){
+            return false;
+        }
+        return left.val == right.val && isSymmetrical(left.left, right.right) && isSymmetrical(left.right, right.left);
+    }
+}
