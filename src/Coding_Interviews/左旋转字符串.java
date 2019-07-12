6f5c974dd7dabb4b2a3c514d377301152b038e29
18645956947 <18645956947@163.com>
2019-07-12 21:16:41 +0800

    剑指offer -- 左旋转字符串

--- src/Coding_Interviews/左旋转字符串.java
+++ src/Coding_Interviews/左旋转字符串.java
@@ -8,14 +8,11 @@ public class ×óÐý×ª×Ö·û´® {
 
     public static String LeftRotateString(String str,int n) {
         int len = str.length();
-        System.out.println(len);
         if(len == 0){
             return "";
         }
         int s = n % len;
-        System.out.println(s);
         String st = str + str;
-        System.out.println(st);
         return st.substring(s, s+len);
     }
 
