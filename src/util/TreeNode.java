1d5aaa9d34462291245faab87e2c8ef6ae935e25
18645956947 <18645956947@163.com>
2019-07-20 11:42:46 +0800

    TreeNode 节点定义

---
+++ src/util/TreeNode.java
@@ -0,0 +1,14 @@
+package util;
+
+/**
+ * TreeNode 节点定义
+ */
+public class TreeNode {
+
+    public int val = 0;
+    public TreeNode left = null;
+    public TreeNode right = null;
+    public TreeNode(int val){
+        this.val = val;
+    }
+}
