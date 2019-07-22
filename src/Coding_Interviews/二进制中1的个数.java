4307c2ec6bd134bf95056125b944eb8f31d9a0ab
18645956947 <18645956947@163.com>
2019-07-22 10:31:06 +0800

    剑指offer -- 二进制中1的个数

--- src/Coding_Interviews/二进制中1的个数.java
+++ src/Coding_Interviews/二进制中1的个数.java
@@ -6,11 +6,18 @@ package Coding_Interviews;
  */
 public class 二进制中1的个数 {
 
-    public int NumberOf1(int n) {
-        return 0;
+    public static int NumberOf1(int n) {
+        String str = Integer.toBinaryString(n);
+        int t = 0;
+        for(int i = 0;i<str.length();i++){
+            if(str.charAt(i) == '1'){
+                t++;
+            }
+        }
+        return t;
     }
 
     public static void main(String[] args) {
-        System.out.println(Integer.toBinaryString(4).getClass());
+        System.out.println(二进制中1的个数.NumberOf1(234));
     }
 }
