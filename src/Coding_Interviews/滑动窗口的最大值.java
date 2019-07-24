3ea1787d895d2c5af67785bbf657e1c2c62070cc
18645956947 <18645956947@163.com>
2019-07-24 09:45:19 +0800

    剑指offer -- 滑动窗口的最大值

--- src/Coding_Interviews/滑动窗口的最大值.java
+++ src/Coding_Interviews/滑动窗口的最大值.java
@@ -25,11 +25,8 @@ public class 滑动窗口的最大值 {
         ArrayList<ArrayList<Integer>> list = new ArrayList<>();
 
         while (r < num.length){
-            int MAX = max(num[l], num[l+1], num[r]);
-//            System.out.println(num[l] +"-----"+num[l+1] +"---"+num[r]);
+            int MAX = max(num[l++], num[l+1], num[r++]);
             res.add(MAX);
-            l++;
-            r++;
         }
         return res;
 
@@ -40,6 +37,6 @@ public class 滑动窗口的最大值 {
 
     public static void main(String[] args) {
         int[] arr = new int[]{2,3,4,2,6,2,5,1};
-        System.out.println(滑动窗口的最大值.maxInWindows(arr, 1));
+        System.out.println(滑动窗口的最大值.maxInWindows(arr, 3));
     }
 }
