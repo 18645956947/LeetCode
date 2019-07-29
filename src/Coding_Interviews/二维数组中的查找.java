8952b0ca5f20c1913c961bbe623270c6b4052ed4
18645956947 <18645956947@163.com>
2019-07-29 09:37:18 +0800

    剑指offer -- 二维数组中的查找

--- src/Coding_Interviews/二维数组中的查找.java
+++ src/Coding_Interviews/二维数组中的查找.java
@@ -29,8 +29,28 @@ public class 二维数组中的查找 {
         return false;
     }
 
+    public static boolean Find02(int target, int [][] array) {
+        if(array == null || array.length == 0){
+            return false;
+        }
+        int row = 0;
+        int col = array[0].length - 1;
+        while(row <= array.length - 1 && col >= 0){
+            if(target == array[row][col]){
+                return true;
+            }
+            else if(target < array[row][col]){
+                col--;
+            }
+            else{
+                row++;
+            }
+        }
+        return false;
+    }
+
     public static void main(String[] args) {
         int[][] a = new int[][]{{1,2,3}, {2,3,4}, {3,4,5}};
-        System.out.println(二维数组中的查找.Find( 5, a));
+        System.out.println(二维数组中的查找.Find02( 5, a));
     }
 }
