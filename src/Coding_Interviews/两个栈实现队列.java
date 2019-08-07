44311053e47c27fd7e427ce721b8eb264c20d40b
18645956947 <18645956947@163.com>
2019-08-07 19:01:20 +0800

    剑指offer -- 用两个栈实现队列

--- src/Coding_Interviews/两个栈实现队列.java
+++ src/Coding_Interviews/两个栈实现队列.java
@@ -3,12 +3,13 @@ package Coding_Interviews;
 import java.util.Stack;
 
 /**
- * 用两个栈实现队列
+ * 剑指offer -- 用两个栈实现队列
  * @author zhx
  * 方法就是stack1 是用来进队列的，
  * 出队列的话 其实就是先入栈一次  再出栈 入栈进stack2时  再调用pop()方法 就相当于先进先出了。
  */
 public class 两个栈实现队列 {
+
     Stack<Integer> stack1 = new Stack<Integer>();
     Stack<Integer> stack2 = new Stack<Integer>();
 
