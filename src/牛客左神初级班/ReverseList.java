b2fdfa6c5ea86b5efd4cb48a0cd1a81c05f01306
18645956947 <18645956947@163.com>
2019-10-21 14:50:31 +0800

    链表反转

--- src/牛客左神初级班/ReverseList.java
+++ src/牛客左神初级班/ReverseList.java
@@ -2,7 +2,46 @@ package 牛客左神初级班;
 
 /**
  * 链表反转
+ * @author zhx
  */
 public class ReverseList {
 
+    public static class Node{
+        public int value;
+        public Node next;
+        public Node(int data){
+            this.value = data;
+        }
+    }
+
+    public static Node reverseList(Node head){
+        if(head == null){
+            return null;
+        }
+        Node pre = null;
+        Node cur = head;
+        Node post;
+        while (cur != null){
+            post = cur.next;
+            cur.next = pre;
+            pre = cur;
+            cur = post;
+        }
+        return pre;
+    }
+    public static void printNode(Node head){
+        Node node = ReverseList.reverseList(head);
+        while (node!=null){
+            System.out.println(node.value);
+            node = node.next;
+        }
+    }
+
+    public static void main(String[] args) {
+        Node head  = new Node(1);
+        head.next = new Node(2);
+        head.next.next = new Node(3);
+        head.next.next.next = new Node(4);
+        ReverseList.printNode(head);
+    }
 }
