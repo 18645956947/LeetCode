3fc17a9d7fe7228b75a0326fa873167110f1a514
18645956947 <18645956947@163.com>
2019-10-21 14:51:14 +0800

    链表反转

--- src/牛客左神初级班/ReverseList.java
+++ src/牛客左神初级班/ReverseList.java
@@ -38,6 +38,7 @@ public class ReverseList {
     }
 
     public static void main(String[] args) {
+
         Node head  = new Node(1);
         head.next = new Node(2);
         head.next.next = new Node(3);
