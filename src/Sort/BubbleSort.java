2d14412652de3ba9f941dc8ae7b24ea67ace1f97
18645956947 <18645956947@163.com>
2019-11-28 15:36:08 +0800

    冒泡排序

--- src/Sort/BubbleSort.java
+++ src/Sort/BubbleSort.java
@@ -3,30 +3,32 @@ package Sort;
 import java.util.Arrays;
 
 /**
- * @author lenovo
+ * @author zhx
  * @date 2019年3月14日下午10:32:58
- * @Description:
+ * 冒泡排序
+ *
  */
 public class BubbleSort {
 	
-	//冒泡排序
+
 	public static int[] bubble(int[] nums){
-		int temp = 0;
-		for (int i = 0; i < nums.length-1; i++) {
-			for (int j = 0; j < nums.length-i; j++) {
-				if(nums[i] < nums[j]){
-					swap(nums[i], nums[j]);
+		if(nums == null || nums.length <= 2){
+			return nums;
+		}
+		for (int end = nums.length-1; end > 0; end--) {
+			for (int j = 0; j < end; j++) {
+				if(nums[j] > nums[j+1]){
+					swap(nums, j, j+1);
 				}
 			}
 		}
 		return nums;
 	}
 	//交换两个数
-	public static void swap(int a, int b){
-		int temp = 0;
-		temp = a;
-		a = b;
-		b = temp;
+	public static void swap(int[] arr, int a, int b){
+		int temp = arr[a];
+		arr[a] = arr[b];
+		arr[b] = temp;
 	}
 	
 	  //冒泡排序
