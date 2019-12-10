4798975af1c626ed91d5debe1bce8747171a69d9
18645956947 <18645956947@163.com>
2019-12-10 20:15:37 +0800

    不同路径_2

---
+++ src/Dynamic_Programming/不同路径_2.java
@@ -0,0 +1,34 @@
+package Dynamic_Programming;
+
+/**
+ * leetcode - 63 不同路径_2
+ * @author zhx
+ */
+public class 不同路径_2 {
+    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+        int m = obstacleGrid.length;
+        int n = obstacleGrid[0].length;
+        if(obstacleGrid[0][0] == 1){
+            return 0;
+        }
+        int[][] dp = new int[m][n];
+        dp[0][0] = 1;
+        for (int i = 1; i < m; i++) {
+            dp[i][0] = (obstacleGrid[i][0] == 0 && dp[i - 1][0] == 1) ? 1 : 0;
+        }
+        for (int i = 1; i < n; i++) {
+            dp[0][i] = (obstacleGrid[0][i] == 0 && dp[0][i - 1] == 1) ? 1 : 0;
+        }
+        for(int i = 1;i < m;i++){
+            for(int j = 1;j < n;j++){
+                if(obstacleGrid[i][j] == 0){
+                    dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+                }else{
+                    dp[i][j] = 0;
+                }
+
+            }
+        }
+        return dp[m - 1][n - 1];
+    }
+}
