843402031693d8506833bca117708aa30bd7f354
18645956947 <18645956947@163.com>
2020-04-05 00:56:50 +0800

    剑指offer - 合并两个排序的链表

--- src/剑指offer/代码的完整性/合并两个排序的链表.java
+++ src/剑指offer/代码的完整性/合并两个排序的链表.java
@@ -59,4 +59,36 @@ public class 合并两个排序的链表 {
         }
         return root;
     }
+    //递归
+    public ListNode Merge_02(ListNode list1,ListNode list2) {
+        if(list1 == null && list2 == null){
+            return null;
+        }
+        if(list1 == null && list2 != null){
+            return list2;
+        }
+        if(list1 != null && list2 == null){
+            return list1;
+        }
+        return get(list1, list2);
+    }
+    private ListNode get(ListNode list1, ListNode list2){
+        ListNode l1 = list1;
+        ListNode l2 = list2;
+
+        if(l1 == null){
+            return l2;
+        }else if(l2 == null){
+            return l1;
+        }
+        ListNode root = null;
+        if(l1.val >= l2.val){
+            root = l2;
+            root.next = get(l1, l2.next);
+        }else{
+            root = l1;
+            root.next = get(l1.next, l2);
+        }
+        return root;
+    }
 }
